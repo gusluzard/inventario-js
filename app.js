562ddcb9a32838a2e5b8edf6dcae31ce31c30f63
Gustavo Luzardo <gustavo.luzardo987@gmail.com>
2026-09-09 19:17:01 -0500

    Creando el inventario

---
+++ app.js
@@ -0,0 +1,8 @@
+const productos = [
+    {nombre: "Arroz", precio: 1.25, stock: 10 },
+    {nombre: "Aceite", precio: 3.5, stock: 0},
+    {nombre: "Azucar", precio: 1.1, stock: 8},
+    {nombre: "Leche", precio: 1.4, stock: 0},
+    {nombre: "Café", precio: 1.4, stock: 5}
+];
+
