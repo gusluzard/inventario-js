0504e1ab7315b5c50383ef3f046bad4782071945
Gustavo Luzardo <gusluzard@gmail.com>
2026-09-12 21:59:28 -0500

    practivando filter y reduce

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ const productos = [
     {nombre: "Café", precio: 1.4, stock: 5}
 ];
 
+const productosDisponiblesCaros = productos.filter(producto => producto.stock > 0 && producto.precio >= 1.4);
+
+console.log("Productos disponibles y caros:", productosDisponiblesCaros);
+
+const productoMasStock = productos.filter(producto => producto.stock >= 5);
+
+console.log("Productos con stock mayor o igual a 5:", productoMasStock);
+
+const productoEconomicosDisponibles = productos.filter(producto => producto.stock > 0 && producto.precio < 2);
+
+console.log("Productos económicos y disponibles:", productoEconomicosDisponibles);
+
+const totalUnidades = productos.reduce((total, producto) => total + producto.stock, 0);
+
+console.log("Total de unidades en stock:", totalUnidades);
